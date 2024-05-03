b0d3156fbf388ec5191c70dc986e00ec89a69794
SuenderSingh <104889210+SuenderSingh@users.noreply.github.com>
2024-05-03 16:33:44 +0530

    updated code of dashboard ui

--- src/pages/MyStats.tsx
+++ src/pages/MyStats.tsx
@@ -1,22 +1,20 @@
 import { Stack } from "@mui/material";
 import React from "react";
 import AgentDashboard from "src/layouts/newDashboard/agentDashbord/AgentDashboard";
-
 import { useAuthContext } from "src/auth/useAuthContext";
 import DistributorDashboard from "src/layouts/newDashboard/DistribitorDashboard/DistributorDashboard";
 function MyStats() {
   const { user } = useAuthContext();
   return (
     <>
-      <Stack
+      {/* <Stack
         style={{
           overflowY: "auto",
-          maxHeight: "720px",
-          paddingBottom: "100px",
+          // maxHeight: "720px"
         }}
-      >
-        {user?.role == "agent" ? <AgentDashboard /> : <DistributorDashboard />}
-      </Stack>
+      > */}
+      {user?.role == "agent" ? <AgentDashboard /> : <DistributorDashboard />}
+      {/* </Stack> */}
     </>
   );
 }
