8d95c153abddc66b821bab97b65f5991ce14181a
Tramoreact <163147385+Tramoreact@users.noreply.github.com>
2024-09-24 19:27:51 +0530

    Updated distributor dashboard

--- src/pages/MyStats.tsx
+++ src/pages/MyStats.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import AgentDashboard from "src/layouts/newDashboard/agentDashbord/AgentDashboard";
 import { useAuthContext } from "src/auth/useAuthContext";
 import DistributorDashboard from "src/layouts/newDashboard/DistribitorDashboard/DistributorDashboard";
+
 function MyStats() {
   const { user, Api, UploadFileApi } = useAuthContext();
   return (
